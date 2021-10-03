785c91d48fc4ed70ad0384bcefff99457d179156
Ivan Yu <85257356+IvanYu327@users.noreply.github.com>
2021-10-03 13:07:54 -0400

    Update chatbot.py
    edited training model

--- chatbot.py
+++ chatbot.py
@@ -24,16 +24,11 @@ words = []
 classifyingTags = []
 documents = []
 stringsToIgnore = ['?,!']
-print("Looping through the Intents to Convert them to words, classifyingTags, documents and stringsToIgnore.......")
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-        # tokenize each word in the sentence
         w = nltk.word_tokenize(pattern)
-        # add to our words list
         words.extend(w)
-        # add to documents in our corpus
         documents.append((w, intent['tag']))
-        # add to our classifyingTags list
         if intent['tag'] not in classifyingTags:
             classifyingTags.append(intent['tag'])
 
@@ -41,7 +36,6 @@ for intent in intents['intents']:
 words = [stemmer.stem(w.lower()) for w in words if w not in stringsToIgnore]
 words = sorted(list(set(words)))
 
-# remove duplicates
 classifyingTags = sorted(list(set(classifyingTags)))
 
 
@@ -52,17 +46,12 @@ output = []
 output_empty = [0] * len(classifyingTags)
 
 for doc in documents:
-    # initialize our bag of words
     bag = []
-    # list of tokenized words for the pattern
     pattern_words = doc[0]
-    # stem each word
     pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
-    # create our bag of words array
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
 
-    # output is a '0' for each tag and '1' for current tag
     output_row = list(output_empty)
     output_row[classifyingTags.index(doc[1])] = 1
 
@@ -110,18 +99,14 @@ with open('intents.json') as json_data:
     intents = json.load(json_data)
     
 
-# load our saved model
 model.load('./model.tflearn')
 
 
 def clean_up_sentence(sentence):
-    # It Tokenize or Break it into the constituents parts of Sentense.
     sentence_words = nltk.word_tokenize(sentence)
-    # Stemming means to find the root of the word.
     sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
     return sentence_words
 
-# Return the Array of Bag of Words: True or False and 0 or 1 for each word of bag that exists in the Sentence
 def bow(sentence, words):
     sentence_words = clean_up_sentence(sentence)
     bag = [0]*len(words)
@@ -132,44 +117,23 @@ def bow(sentence, words):
     return(np.array(bag))
 
 def classify(sentence):
-    # Prediction or To Get the Posibility or Probability from the Model
     results = model.predict([bow(sentence, words)])[0]
-    # Exclude those results which are Below Threshold
     results = [[i,r] for i,r in enumerate(results) if r>0.25]
-    # Sorting is Done because heigher Confidence Answer comes first.
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
-        return_list.append((classifyingTags[r[0]], r[1])) #Tuppl -> Intent and Probability
+        return_list.append((classifyingTags[r[0]], r[1])
     return return_list
 
 def response(sentence, userID='123'):
     results = classify(sentence)
-    # That Means if Classification is Done then Find the Matching Tag.
     if results:
-        # Long Loop to get the Result.
         while results:
             for i in intents['intents']:
-                # Tag Finding
                 if i['tag'] == results[0][0]:
-                    # Random Response from High Order Probabilities
                     
                     temp = random.choice(i['responses'])
                     print("RETURNING "+temp)
                     return temp
 
             results.pop(0)
-
-
-# while True:
-#     input_data = input("You- ")
-#     answer = response(input_data)
-#     answer
-
-# async def chatAI(message):
-    
-#     print("start AI")
-#     print(message)
-#     responseMSG = response(message)
-#     print(responseMSG)
-#     return responseMSG
